a1a5dd2ee0cdac423f630915872f7abbe91b1394
f1veStar3 <75863469+f1veStar3@users.noreply.github.com>
2024-10-22 20:17:56 +0300

    fix test.py

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
-from .models import Cart, Order, Color, CartItem, Product, Brand,Category
+from .models import Cart, Order
 
 class BasicViewsTest(TestCase):
     def setUp(self):
